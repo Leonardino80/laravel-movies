007b0b3894dd93f70310f38311581bb5cd6b231a
Leonardino80 <gilgamesh2005@gmail.com>
2019-07-15 04:49:15 +0200

    genre_id

---
+++ database/migrations/2019_07_15_020054_update_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            $table->bigInteger('genre_id')->unsigned();
+            $table->foreign('genre_id')->references('id')->on('genres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            // modo standard di chiamare una foreign key
+            $table->dropForeign('movies_genre_id_foreign');
+            $table->dropColumn('genre_id');
+        });
+    }
+}
